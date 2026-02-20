22adb0df00dc3b59a3d0a0f66e60a3c17a34e00c
527cbowman-sys <527cbowman@frhsd.com>
2026-02-20 10:35:22 -0500

    Refactor calculator styles and enhance functionality; update button hover effects and improve number input handling

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+// WHAT USER IS CURRENTLY TYPING
+let typedNumberText = ''
+
+// THE NUMBER WE STORE FOR CALCULATIONS
+let storedNumber = null
+
+// THE OPERATOR CURRENTLY SELECTED (+ - * /)
+let currentOperator = ''
+
+// USED ONLY FOR DISPLAYING THE HISTORY LINE
+// EXAMPLE: ["3", "+", "4"]
+let historyParts = []
+
+
+function setStatus(message){
+    document.getElementById('statusLine').innerText = message
+}
+
+function showSymbol(op){
+    if (op === '*') return '×';
+    if (op === '/') return '÷';
+    if (op === '-') return '&#x2212';
+    return op;
+}
+
+function updateScreen(){
+    const display = document.getElementById('displayLine')
+    const history = document.getElementById('historyLine')
+    const status = document.getElementById('statusLine')
+
+    display.textContent = typedNumberText
+}
+
+
 function pressNumber(digit) {
-    console.log(digit)
+    setStatus('')
+
+    //STORE NUMBER BEING PRESSED
+    if (typedNumberText === '0') {
+        typedNumberText = digit;
+    } else {
+        typedNumberText = typedNumberText + digit
+    }
+
+    updateScreen()
 }
